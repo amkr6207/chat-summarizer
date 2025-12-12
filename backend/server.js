9ef5c6c4e41855914492f6e3eb159bbf47a4f854
Aman <amkr6207@gmail.com>
2025-12-12 16:53:05 +0530

    add root directory

--- backend/server.js
+++ backend/server.js
@@ -74,6 +74,13 @@ app.use((err, req, res, next) => {
     });
 });
 
+
+app.get("/", (req, res) => {
+    res.send("Chat Summarizer Backend is running successfully!");
+});
+
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 
